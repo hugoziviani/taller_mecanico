84cb1a5b062603b191230efb6f7ec5ea57fbc84b
Hugo Ziviani <hugoziviani@hotmail.com>
2020-08-26 08:19:54 -0300

    criando repositório git

---
+++ mecanico/mecanico.h
@@ -0,0 +1,10 @@
+//
+// Created by Hugo Ziviani on 8/25/20.
+//
+
+#ifndef TALLER_MECANICO_MECANICO_H
+#define TALLER_MECANICO_MECANICO_H
+#include <iostream>
+using namespace std;
+
+#endif //TALLER_MECANICO_MECANICO_H
